633840d7f2ad5d89570382882e5de48e2c728cfa
Kağan Demirhindi <kadenn@kagintosh.local>
2021-07-04 19:26:39 +0300

    fixed security vulnerability

--- client/src/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyDOYP_coppVKwINqTOSDjj4x49ysvWuETE',
-  authDomain: 'social-media-app-5b2b2.firebaseapp.com',
-  projectId: 'social-media-app-5b2b2',
-  storageBucket: 'social-media-app-5b2b2.appspot.com',
-  messagingSenderId: '691536010541',
-  appId: '1:691536010541:web:bdf8e0c889fcda02d0c7a4',
-};
